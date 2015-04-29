c1d77c46585be3c8badb0bad946dcf6c3b58eb04
Lukas Hellebrandt <lhellebr@redhat.com>
2015-04-29 02:20:16 +0200

    Ucet maintenance; Authentication security

--- project/src/org/fit/pis/back/UcetBean.java
+++ project/src/org/fit/pis/back/UcetBean.java
@@ -1,13 +1,17 @@
 package org.fit.pis.back;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
-import org.fit.pis.data.Osoba;
 import org.fit.pis.data.Ucet;
+import org.fit.pis.data.Ucet.Opravneni;
 import org.fit.pis.service.UcetManager;
 
 
@@ -15,10 +19,16 @@ import org.fit.pis.service.UcetManager;
 @SessionScoped
 public class UcetBean
 {
+	// private static final Logger log = Logger.getLogger( UcetBean.class.getName() );
+	// --- log.log( Level.WARNING, "!!!");
 	@EJB
 	private UcetManager ucetMgr;
 	private Ucet ucet;
     
+    public Opravneni[] getEnumOpravneni(){
+    	return Opravneni.values();
+    }
+	
 	public UcetManager getUcetMgr() {
 		return ucetMgr;
 	}
@@ -44,6 +54,16 @@ public class UcetBean
 		ucet = new Ucet();
 		return "new";
 	}
+	
+	public String actionInsertNew()
+    {
+		if(ucetMgr.find(ucet.getLogin())!=null){
+			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("Ucet s timto jmenem jiz existuje!"));
+			return "new";
+		}
+        ucetMgr.save(ucet);
+        return "insert";
+    }
 	
     public String actionEdit(Ucet ucet)
     {
--- project/src/org/fit/pis/data/Ucet.java
+++ project/src/org/fit/pis/data/Ucet.java
@@ -1,16 +1,39 @@
 package org.fit.pis.data;
 
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.xml.bind.DatatypeConverter;
 
 @Entity
 @Table(name = "ucet")
 public class Ucet {
+	private static final Logger log = Logger.getLogger( Ucet.class.getName() );
+
 	public enum Opravneni{
-		ADMINISTRATOR, UREDNIK, POLICISTA
+		ADMINISTRATOR("Administrátor"), UREDNIK("Úředník"), POLICISTA("Policista");
+	    private String label;
+
+	    private Opravneni(String label) {
+	        this.label = label;
+	    }
+
+	    public String getLabel() {
+	        return label;
+	    }
 	}
 	
 	@Id
@@ -28,8 +51,19 @@ public class Ucet {
 	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password) {
-		this.password = password;
+	public void setPassword(String password){
+		byte[] salt;
+		try {
+			salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(32);
+			KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 512);
+			SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+			byte[] hash = f.generateSecret(spec).getEncoded();
+			this.password = DatatypeConverter.printBase64Binary(salt) + '$' + DatatypeConverter.printBase64Binary(hash);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
 	}
 	public Opravneni getOpravneni() {
 		return opravneni;
@@ -38,6 +72,24 @@ public class Ucet {
 		this.opravneni = opravneni;
 	}
     public boolean verifyPassword(String password){
-    	return password.equals(this.password);
+    	try {
+			String[] parts = this.password.split("\\$");
+			log.log(Level.WARNING, "{0}      {1}", new Object[]{parts[0],parts[1]});
+			byte[] salt = DatatypeConverter.parseBase64Binary(parts[0]);
+			byte[] hash = DatatypeConverter.parseBase64Binary(parts[1]);
+			log.log(Level.WARNING, "{0}      {1}", new Object[]{Arrays.toString(salt),Arrays.toString(hash)});
+			KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 512);
+			SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+			byte[] realHash = f.generateSecret(spec).getEncoded();
+			log.log(Level.WARNING, "{0}", new Object[]{Arrays.toString(realHash)});
+	    	return Arrays.equals(hash,realHash);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return false;
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+			return false;
+		}
     }
+
 }
